b3bf48e573499cb0ec647f764a7ac97eb95dc039
Nick Trunov <trunov1997@yandex.ru>
2020-12-17 23:08:17 +0300

    Added text formatting and edits

--- src/index.js
+++ src/index.js
@@ -8,10 +8,6 @@ var W = localStorage.getItem('W');
 var A = localStorage.getItem('A');
 var n = localStorage.getItem('n');
 var I = localStorage.getItem('I');
-var C = localStorage.getItem('C');
-var P = localStorage.getItem('P');
-var L = localStorage.getItem('L');
-var D = localStorage.getItem('D');
 
 if (W > 0) $('.W').val(W);
 else $('.W').val('');
@@ -21,14 +17,29 @@ if (n > 0) $('.n').val(n);
 else $('.n').val('');
 if (I > 0) $('.I').val(I);
 else $('.I').val('');
-if (C > 0) $('.C').text(C + ' ₽');
-else $('.C').text('0 ₽')
-if (P > 0) $('.P').text(Math.round(P) + ' ₽');
-else $('.P').text('0 ₽')
-if (L > 0) $('.L').text(Math.round(L) + ' ₽');
-else $('.L').text('0 ₽')
-if (D > 0) $('.D').text(Math.round(D) + ' ₽');
-else $('.D').text('0 ₽')
+
+function calcCredit () {
+    var C = W - A;
+    var P = C * ((I / 1200) + ((I / 1200) / ((1 + (I / 1200)) ** (n * 12) - 1)));
+    var L = P * n * 12 - C;
+    var D = 5 * P / 3;
+
+    if (n == '') {
+        P = '';
+        D = '';
+    }
+
+    if (C > 0) $('.C').text(C.toLocaleString() + ' ₽');
+    else $('.C').text('0 ₽')
+    if (P > 0) $('.P').text(Math.round(P).toLocaleString() + ' ₽');
+    else $('.P').text('0 ₽')
+    if (L > 0) $('.L').text(Math.round(L).toLocaleString() + ' ₽');
+    else $('.L').text('0 ₽')
+    if (D > 0) $('.D').text(Math.round(D).toLocaleString() + ' ₽');
+    else $('.D').text('0 ₽') 
+}
+
+calcCredit();
 
 $('.button--save').text(localStorage.getItem('buttonSave'));
 
@@ -39,19 +50,7 @@ $('input').keyup(function () {
     I = $('.I').val();
     n = $('.n').val();
 
-    C = W-A;
-    P = C * ((I / 1200) + ((I / 1200) / ((1 + (I / 1200)) ** (n * 12) - 1)));
-    L = P * n * 12 - C;
-    D = 5 * P / 3;
-
-    if (C > 0) $('.C').text(C + ' ₽');
-    else $('.C').text('0 ₽')
-    if (P > 0) $('.P').text(Math.round(P) + ' ₽');
-    else $('.P').text('0 ₽')
-    if (L > 0) $('.L').text(Math.round(L) + ' ₽');
-    else $('.L').text('0 ₽')
-    if (D > 0) $('.D').text(Math.round(D) + ' ₽');
-    else $('.D').text('0 ₽')
+    calcCredit();
 
     if (W == localStorage.getItem('W') && A == localStorage.getItem('A') && I == localStorage.getItem('I') && n == localStorage.getItem('n')) $('.button--save').text('Saved');
 });
@@ -66,10 +65,6 @@ $('.button--clear').on('click', function () {
     A = '';
     n = '';
     I = '';
-    C = '';
-    P = '';
-    L = '';
-    D = '';
     localStorage.clear();
     localStorage.setItem('buttonSave', 'Save');
     $('.button--save').text(localStorage.getItem('buttonSave'));
@@ -80,10 +75,12 @@ $('.button--save').on('click', function () {
     localStorage.setItem('A', A);
     localStorage.setItem('I', I);
     localStorage.setItem('n', n);
-    localStorage.setItem('C', C);
-    localStorage.setItem('P', P);
-    localStorage.setItem('L', L);
-    localStorage.setItem('D', D);
     localStorage.setItem('buttonSave', 'Saved');
     $('.button--save').text(localStorage.getItem('buttonSave'));
+});
+
+$('input').bind("change keyup input click", function () {
+    if (this.value.match(/[^0-9]/g)) {
+        this.value = this.value.replace(/[^0-9]/g, '');
+    }
 });
